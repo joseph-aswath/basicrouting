c3472f4e62980daca3bcd3ded8ef2e55be9ca1ce
joseph-aswath <josehaswath@gmail.com>
2022-05-04 13:04:33 +0530

    initiated project with basic html & css

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+//import { Routes,Route,Outlet,Link} from 'react-router-dom';
+/******************************************************************* */
+export default function App(){
+    return(
+        <div>
+            <div className="mainHeading">
+            <h1>BASIC ROUTING</h1>
+            <hr></hr>
+            </div>
+        </div>
+    );
+}
+/********************************************************************* */
